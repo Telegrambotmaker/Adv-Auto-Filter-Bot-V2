1f2fb019fe18c14e74dae9a1ead339d67147c043
Telegrambotmaker <101441413+Telegrambotmaker@users.noreply.github.com>
2022-03-12 13:05:43 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/moviekings12"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/moviekings12'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/+NH4YcpR9ld9iNWM9')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/+NH4YcpR9ld9iNWM9')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]

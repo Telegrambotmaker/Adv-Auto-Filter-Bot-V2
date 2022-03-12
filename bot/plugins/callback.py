7ba7e72e493bf4a49fbb0b7cef2b258a270f5ab7
Telegrambotmaker <101441413+Telegrambotmaker@users.noreply.github.com>
2022-03-12 13:03:44 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Filter-Bot-V2')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/tamilbotss'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://t.me/+NH4YcpR9ld9iNWM9')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/tamilbotss')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
